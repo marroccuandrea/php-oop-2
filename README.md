2d867c5efdf87b00eba93856c077f54e7edacad7
marroccuandrea <marroccuandrea@yahoo.it>
2024-05-03 15:23:24 +0200

    First commit - struttura html base

---
+++ README.md
@@ -0,0 +1,9 @@
+PHP OOP 2
+===
+## Descrizione
+Immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+- L’e-commerce vende prodotti per animali.
+- I prodotti sono categorizzati, le categorie sono Cani o Gatti.
+- I prodotti saranno oltre al cibo, anche giochi , cucce , etc.
+Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia).<br>
+Nel “modellizzare” i dati pensate alla differenza che c’è fra prodotti ( cibo,  giochi , cucce…) e categorie (cani, gatti…) e come rappresentarli
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/data/db.php';
+require_once __DIR__ . '/Model/Cat-shop.php';
+require_once __DIR__ . '/Model/Dog-shop.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP OOP 2</title>
+</head>
+
+<body>
+    <div class="container my-5 text-center">
+        <h1>Pets Shop</h1>
+    </div>
+    <div class="container my-5">
+        <h2>Cats shop: </h2>
+    </div>
+    <div class="container my-5">
+        <h2>Dogs shop: </h2>
+    </div>
+</body>
+
+</html>
